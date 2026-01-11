94b295d4bbf9324df25cce1f0b0515220a0dd5a1
LaurenMBell <bell.laurenmar@gmail.com>
2026-01-10 16:18:22 -0800

    added report and first successful pls-cpx ipuc (although way wrong lol)

--- inverse_puc/reverse_puc.py
+++ inverse_puc/reverse_puc.py
@@ -2,6 +2,7 @@ import networkx as nx
 import pandas as pd
 import matplotlib.pyplot as plt
 import igraph as ig
+import time
 
 
 #CAN THIS BE REWORKED WITH IGRAPH??
@@ -21,10 +22,6 @@ import igraph as ig
 5) frustration = proportion of disagreements, if that proportion is >0.2, 
     remove the node
 """
-
-def assign_node_dir(df, node):
-    df[node]
-
 
 def vote(G, neighbor, target):
     #up = 1, down = -1
@@ -62,20 +59,20 @@ def define_layers(G, l0, l1):
     return layers
 
 
-def reverse_puc(G, ln, lm, thresh=0.2):
+def reverse_puc(G, ln, lm, f, thresh=0.2):
     #function to take each node in a layer and find directionality for it
     to_remove_nodes = set() #set to collect nodes to remove instead of during iteration
     #to_remove_edges = set()
 
     for node in ln:
-        print(f"NODE: {node}")
+        f.write(f"NODE: {node}\n")
 
         up = 0
         up_e = []
         down = 0
         down_e = []
 
-        for neighbor in G.neighbors(node):
+        for neighbor in list(G.neighbors(node)):
             if neighbor in lm:
                 #every neighbor in lm should vote 
                 n_vote = vote(G, neighbor, node)
@@ -87,35 +84,35 @@ def reverse_puc(G, ln, lm, thresh=0.2):
                     down_e.append(neighbor)
 
         score = up - down
-        print(f"SCORE: {score}")
-        if score == 0: #tie
+        f.write(f"SCORE: {score}\n")
+        if score == 0: #tie or no neighbors
             to_remove_nodes.add(node)
-            print("score == 0, node to be removed")
+            f.write("score == 0, node to be removed\n\n")
             continue
         elif score < 0:
             dir = -1
-            f = up/(up+down)
+            frustration = up/(up+down)
 
             for neighbor in up_e:
                 G.remove_edge(node, neighbor)
-                print(f"removing edge {node} - {neighbor}")
+                f.write(f"removing edge {node} - {neighbor}\n")
 
         elif score > 0:
             dir = 1
-            f = down/(up+down)
+            frustration = down/(up+down)
 
             for neighbor in down_e:
                 G.remove_edge(node, neighbor)
-                print(f"removing edge {node} - {neighbor}")
+                f.write(f"removing edge {node} - {neighbor}\n")
 
         
 
-        if f > thresh: #if frustration is > 0.2, remove node
+        if frustration > thresh: #if frustration is > 0.2, remove node
             to_remove_nodes.add(node)
-            print("f > thresh, node to be removed")
+            f.write("f > thresh, node to be removed\n\n")
         else:
             G.nodes[node]['dir'] = dir
-            print("node updated!")
+            f.write("node updated!\n\n")
 
         
     
@@ -123,48 +120,81 @@ def reverse_puc(G, ln, lm, thresh=0.2):
 
     return ln - to_remove_nodes
 
-def pls_cpx_rpuc():
-    print("started pls-cpx")
+def pls_cpx_rpuc(f):
+    f.write(f"CURRENT TIME: {time.localtime}\n\n")
+    f.write("Started PLS-CPX!\n")
     pls_cpx = pd.read_csv("PLS-CPX_edges.csv")
+    pls_cpx = pls_cpx[pls_cpx["Consistent"] == True]
+
     pls = pd.read_csv("PLS_edges.csv")
+    pls = pls[pls["Consistent?"] == True]
+    cpx_node_dir = pd.read_csv("network_nodes.csv")
     
     l0 = set(pls_cpx['cpx_gene']) #going to be the cpx nodes in pls-cpx
     l1 =  set(pls_cpx['pls_metabolite']) #going to be pls nodes in pls-cpx
 
-    #you need to init 'dir' for all edges and nodes
+    #node direction hash table (gene:dir)
+    dirs = dict(zip(cpx_node_dir["ID"], cpx_node_dir["Mean Log2 Fold Change Direction (DSS)"]))
+
+    #init dir for all edges and cpx nodes
     G = nx.Graph()
 
+    #init L0 and L1 from the PLS-CPX file
     for i, r in pls_cpx.iterrows():
-        G.add_edge(r["cpx_gene"], r["pls_metabolite"], dir=r["edge_dir"])
+        G.add_edge(r["cpx_gene"], r["pls_metabolite"], dir=r["edge_dir"]) 
 
-    for node in G.nodes: #FIX THIS ONLY ASSIGN DIR TO CPX GENES
-        G.nodes[node]["dir"] = G.nodes[node].get("dir", 1)
+    #add node DOC from netwrok_nodes
+    for node in G.nodes: 
+        if node in l0:
+            G.nodes[node]["dir"] = dirs.get(node, None)
 
-    for i, r in pls.iterrows(): #not calculated, need to remake FDR table with pooled_r to get 
-        G.add_edge(r["n1"], r["n2"], dir=r["edge_dir"])
+    #init edges for rest of PLS
+    #TO DO NEED TO DOUBLE CHECK POOLED R CALCULATIONS FOR PLS EDGES !!! DO NOT FORGET TO DO THIS
+    for i, r in pls.iterrows(): 
+        G.add_edge(r["Metabolite 1"], r["Metabolite 2"], dir=r["edge_dir"])
 
+    #define the rest of the graph
     layers = define_layers(G, l0, l1)
     
+    #perform reverse_puc for all layers
     for i in range(1, len(layers)):
-        layers[i] = reverse_puc(G, layers[i], layers[i-1])
+        layers[i] = reverse_puc(G, layers[i], layers[i-1], f)
 
-    #show the graph at the end
+    #to figure out later, it looks terrifying rn
     """
+    #show the graph at the end
     nx.draw(G, with_labels=True, font_weight='bold')
     name = input("name the graph: ")
     plt.savefig(f"{name}.png")
     plt.show() 
-    """
+    """ 
+
+    f.write("FINAL NETWORK NODES:\n")
+    for node in G.nodes:
+        f.write(f"{node}: {G.nodes[node]}\n")
+
+    #save edge table 
+    edges = []
+    for u, v, d in G.edges(data=True):
+        edges.append({"n1": u,"n2": v,"edge_dir": d.get("dir", None)})
+    pd.DataFrame(edges).to_csv("pls_cpx_rpuc_edges.csv", index=False)
+    
+    #save node table 
+    nodes = []
+    for n, d in G.nodes(data=True):
+        if n not in l0:
+            nodes.append({"node": n,"node_dir": G.nodes[n]["dir"]})
+    pd.DataFrame(nodes).to_csv("pls_rpuc_nodes.csv", index=False)
+    
 
 def build_graph(edges):
     #function to make a graph from input edge table, and define L0 
-    G = nx.Graph()
-
-
+    G = nx.Graph() #clearly just do this later 
     return G
 
 def main():
-    pls_cpx_rpuc()
+    f = open("report.txt", 'w')
+    pls_cpx_rpuc(f)
 
 if __name__=="__main__":
     main()
