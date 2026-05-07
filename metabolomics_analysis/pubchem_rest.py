3a3266af9ac92bcf57414e8d782f3aefcc6ee2dd
LaurenMBell <bell.laurenmar@gmail.com>
2026-05-07 10:33:47 -0700

    metabolite ID mapping

--- metabolomics_analysis/pubchem_rest.py
+++ metabolomics_analysis/pubchem_rest.py
@@ -3,18 +3,25 @@ import pubchempy as pcp
 
 mets = pd.read_excel("metabolites_for_sara.xlsx")
 
+mapping = pd.read_csv("pubchemID_mapping.csv", dtype=str)
+mapping = mapping.set_index("Query")
+
 cids = []
 for name in mets["Metabolite Name"]:
     results = pcp.get_compounds(name, "name")
     if results:
-        print(f"{name}: {results[0].cid}")
-        cids.append({"Metabolite Name": name, "PubChem": results[0].cid, "SMILES":results[0].smiles})
+        pubchem_id = str(results[0].cid)
+        hmdb_code = None
+        if pubchem_id in mapping.index:
+            hmdb_code = mapping.at[pubchem_id, "HMDB"]
+        print(f"{name}: {pubchem_id}")
+        cids.append({
+            "Metabolite Name": name, "PubChem": pubchem_id,
+            "HMDB": hmdb_code, "SMILES": results[0].smiles})
     else:
-        #try: 
-            #suffixes = name.strip("")
-            #name = 
         print(f"SKIPPED: {name}")
-        cids.append({"Metabolite Name": name, "PubChem": None, "SMILES":None})
+        cids.append({"Metabolite Name": name, "PubChem": None,
+            "HMDB": None,"SMILES": None})
 
 results_df = pd.DataFrame(cids)
 results_df.to_csv("metabolites_for_sara.csv", index=False)
