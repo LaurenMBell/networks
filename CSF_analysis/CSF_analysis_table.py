978deb0b6ef9038dfabc95ce929ca5de9b1ea2a7
LaurenMBell <bell.laurenmar@gmail.com>
2026-06-11 09:24:00 -0700

    updated final table

--- CSF_analysis/CSF_analysis_table.py
+++ CSF_analysis/CSF_analysis_table.py
@@ -1,14 +1,4 @@
 """
-import pandas as pd 
-
-csf_str_edges = pd.read_csv("../CSF-STR/CSF-STR_FDR_threshold.csv")
-csf_rbn_edges = pd.read_csv("../CSF-RBN/CSF-RBN_FDR_threshold.csv")
-csf_ctx_edges = pd.read_csv("../CSF-CTX/CSF-CTX_FDR_threshold.csv")
-
-csf_str_pred_signs = pd.read_csv("../CSF_ESP/CSF-brain_edge_signs/csf_str_predicted_signs.csv")
-csf_rbn_pred_signs = pd.read_csv("../CSF_ESP/CSF-brain_edge_signs/csf_rbn_predicted_signs.csv")
-csf_ctx_pred_signs = pd.read_csv("../CSF_ESP/CSF-brain_edge_signs/csf_ctx_predicted_signs.csv")
-
 final_table = pd.DataFrame([["n1", "n2", "VECPAC_r", "LPS_r", "DSS_r", "pooled_r", 
              "pred_sign", "model_agreement_pooled_sign", 
              "model_agreement_pred_sign", "pooled_pred_agreement_sign", 
@@ -41,14 +31,17 @@ df = edges[["n1", "n2", "subnetwork", "VECPAC_r", "LPS_r", "DSS_r", "Pooled_r", 
     how="left")
 
 df["pooled_r"] = df["Pooled_r"]
-df["model_sign_num"] = df["Sign"].map({"+": 1, "-": -1})
-df["pooled_r_sign"] = df["pooled_r"].apply(lambda x: 1 if x > 0 else -1)
+df["model_sign"] = df["Sign"].map({"+": 1, "-": -1})
 
+for i in df.index:
+    if df.loc[i, "pooled_r"] > 0:
+        df.loc[i, "pooled_r_sign"] = 1
+    else:
+        df.loc[i, "pooled_r_sign"] = -1
 
-
-df["model_agreement_pooled_sign"] = (df["model_sign_num"] == df["pooled_r_sign"]).map({True: 1, False: -1})
-df["model_agreement_pred_sign"] = (df["model_sign_num"] == df["predicted_sign"]).map({True: 1, False: -1})
-df["pooled_pred_agreement_sign"] = (df["pooled_r_sign"] == df["predicted_sign"]).map({True: 1, False: -1})
+df["model_agreement_pooled_sign"] = (df["model_sign"] == df["pooled_r_sign"]).map({True: 1, False: -1}, na_action='ignore')
+df["model_agreement_pred_sign"] = (df["model_sign"] == df["predicted_sign"]).map({True: 1, False: -1}, na_action='ignore')
+df["pooled_pred_agreement_sign"] = (df["pooled_r_sign"] == df["predicted_sign"]).map({True: 1, False: -1}, na_action='ignore')
 
 df["all_sign_agreement"] = ((df["model_agreement_pooled_sign"] == 1) &
     (df["model_agreement_pred_sign"] == 1) & 
